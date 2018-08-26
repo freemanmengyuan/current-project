60e65338655ef6bd4ce4fb56031b04e338e68fd9
freemanmengyuan <1647786154@qq.com>
2018-08-27 00:09:37 +0800

    cat

--- cvabaidu/conf/db.conf.py
+++ cvabaidu/conf/db.conf.py
@@ -1,19 +1,19 @@
 [gpbzx]
-host = 10.174.92.249
+host =
 username = root
-password = MhxzKhl
-database = gpbzx
+password =
+database =
 charset = utf8
 port = 3306
 
 [nowRedis]
 host = 10.174.92.249
 port = 6379
-auth = MhxzKhl&gpb%%.1112
+auth =
 index = 1
 
 [allRedis]
 host = 10.25.92.228
 port = 6379
-auth = MhxzKhl&gpb%%.1112
+auth =
 index = 2
